5163d87a6d19d346a524245c0cf1f795787b4467
Adam Wrona <adamwronowy@gmail.com>
2025-05-14 12:53:41 +0800

    initial commit – generated spring boot project

---
+++ src/main/java/com/example/horus/HorusAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.horus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorusAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/horus/HorusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.horus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
